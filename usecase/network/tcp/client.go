ef3988e08c2f969dd1482c859eb58396041a5aab
sakuriver <sakuriver.redmine@gmail.com>
2022-07-22 10:02:29 +0900

    Update client.go

--- usecase/network/tcp/client.go
+++ usecase/network/tcp/client.go
@@ -12,7 +12,11 @@ func main() {
 		return
 
 	}
+	// ダイアルした結果のipアドレス情報を取得
+	println("リモート情報")
 	println(conn.RemoteAddr().String())
+	println("ローカルアドレス情報")
+	println(conn.LocalAddr().String())
 
 	fmt.Fprintf(conn, "GET / Http/1.0")
 }
